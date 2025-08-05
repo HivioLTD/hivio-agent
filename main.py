65d3d5d72a00eb1075a2e84aaa4ffb91abf858f9
HivioLTD <hivioltd@gmail.com>
2025-08-05 13:56:33 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from openbb import obb
 
 app = Flask(__name__)
 
@@ -8,14 +7,8 @@ def home():
     return "Привет! ИИ-агент hivio работает!"
 
 @app.route("/nasdaqcm")
-def get_companies():
-    screener = obb.equity.screener
-    results = screener.screener(
-        exchange="NASDAQCM",
-        filters={"roe": [">15"], "market_cap": [">5000000", "<50000000"]},
-        limit=10
-    )
-    return results.to_string()  # можно форматировать иначе
+def nasdaqcm():
+    return "Тут будет список компаний с биржи NasdaqCM"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
